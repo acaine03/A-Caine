94b9b2924882f60bf0d15311e5ba3f724ac1b257
Arthur Caine III <acaine03@gmail.com>
2020-02-07 20:56:31 -0600

    Add more notes.

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-// create a variable called "name" that references a string
-const name = "Caine";
+// 'const' is not about IMMUTABILITY (ability to change) - it's about NO REASSIGNMENT.
+// OBJECTS can change. PRIMITIVES cannot.
+const me = {};
 
-// create a variable called "favoriteSong" that references a string
-let favoriteSong = "song"
+// I CANNOT REASSIGN 'me'.
+// me = "23";
 
-// create a variable called "wage" that references a number
-let wage = 200
+// I CAN reassign the VALUE of 'key' inside of 'me'.
+me.name = "Caine";
 
-// create a variable called "age" that references a number
-let age = 30
+const someone = {
+  name: "III",
+  age: 21,
+  pets: {
+    cat: "Dora",
+    dog: "Fido"
+  }
+};
 
-// create a variable called "onlyChild" that references a boolean
-onlyChild = true
+someone.name = "Ed";
 
-// create a variable called "satisfied" that references a boolean
-satisfied = false
+console.log(someone.pets.cat);
 
-// create a variable called "favoriteThing" that references ANY primitive value
-let favoriteThing = "toy";
-
-// print the data type of "favoriteThing"
-console.log(favoriteThing);
-
-// ! You heard a new song that became your favorite.
-// Reassign the variable "favoriteSong"
-let favoriteSong = "new song";
-
-// ! You no longer like the type of data stored in "favoriteThing"
-// Reassign "favoriteThing" to a different primitive data type and print its type
-let favoriteThing = 15;
-
-// ! Happy Birthday!
-// Reassign "age" to a new value
-let age = 31;
-
-// ! You got a big raise at work.
-// Reassign "wage" to a new value
-let wage = 150;
-
-// ! Your raise changed your job satisfaction.
-// Assign "satisfied" to true
-satisfied = false
-
-// use "console.log()" and "+" to print a sentence that includes two variables
-console.log(${name} ${age});
+someone.name = "Ed";
+someone.occupation = "dev";
