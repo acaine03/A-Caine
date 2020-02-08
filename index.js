13ccfadced7706a60459cb539c4bd8a0039b344a
Arthur Caine III <acaine03@gmail.com>
2020-02-07 20:09:43 -0600

    Add notes.

---
+++ index.js
@@ -0,0 +1,54 @@
+// const pen = "pen";
+
+// const x = 3;
+
+// const name = "Caine";
+
+// We reserve some space in memory and 'name' it 'numOfYears'
+// let numOfYears = 9;
+
+// 'let' allows us to re-assign.
+// This REFERENCES 'numOfYears' in memory - RIPS OUT the '9' and REPLACES it with a '10'.
+// numOfYears = 10;
+
+// JS is a DYNAMICALLY TYPED LANGUAGE.
+// numOfYears = "ten";
+
+// Create a sentence that says the name and the number or years of experience.
+// The '+' is CONCATENATING our STRINGS.
+// '.' notation is used to access KEYS within an OBJECT.
+// 'log' is a METHOD.
+// We INVOKE the METHOD with ().
+// We pass in an ARGUMENT.
+// console.log("My name is " + name + ".");
+
+// Template literals use INTERPOLATION
+// INTERPOLATION occurs inside the '${}'.
+
+// console.log(`My name is ${name}`);
+
+// creat a variable "timeOfDay" and assign it a string.
+// const timeOfDay = "Evening";
+
+// create a variable "greeting" that uses a template literal to 'Good' followed by 'timeOfDay'.
+// const greeting = `Good ${timeOfDay}`;
+
+// print "greeting"
+// console.log(greeting);
+// console.log(`Good ${timeOfDay}`);
+
+// create a new variable, but do not assign it a value
+// let something = undefined; // NEVER DO THIS!!
+
+// 'null' represents the deliberate assignment of a 'nothing' value.
+// let someone = null; //RARELY DO THIS!
+
+// const useless = null;
+
+// print the variable and its type
+// ? what type should we expect?
+// console.log(something);
+// console.log(someone);
+// console.log(somethingElse);
+
+// null is the 5th and final primitive data type.
