e9f4db604ea2422eef2a07a49e3358beb6d01af3
Arthur Caine III <acaine03@gmail.com>
2020-02-11 21:34:18 -0600

    Create html and js with browswer sync.

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const me = {
-  age = 36
+const kitten = "meow"
+const cat = => {
+  console.log(kitten)
 }
-
-console.log(me.age);
-
-console.log(me["age"]);
-
-let keyINeed = "age";
-
-console.log(me.keyINeed);
-
-// I must use BRACKET NOTATION if I need to use a VARIABLE for my key.
+cat()
