9fe74ff2aec348d69430a2da8c17ccc6ee51aac8
Arthur Caine III <acaine03@gmail.com>
2020-02-12 21:22:30 -0600

    Add more exercises.

--- index.js
+++ index.js
@@ -100,3 +100,39 @@
 // add a line to the function's body to print the value of the rest operator
 
 // again, invoke the function and pass in more than two numbers
+
+// wrap the code below in a function called "addNumbersFrom1To100" and return the total
+// ! do not use console.log inside your function
+
+// let total = 0;
+// for (let i = 1; i <= 100; i++) {
+//   total += i;
+// }
+
+// invoke the function and console.log its value
+// function addNumbersFrom1To100() {
+//   let total = 0;
+//   for (let i = 1; i <= 100; i++) {
+//     total += i;
+//   }
+// }
+// try to print the variable "total"
+// console.log(total);
+// ? what do you expect ?
+
+// examine the code below,
+// ? which console.log statement will not print when you invoke the parentAndChildScope function ?
+function childScope(param) {
+  const childVariable = "I am in the child's scope";
+  console.log("Child Scope, Parent Variable:", param);
+  console.log("Child Scope, Child Variable:", childVariable);
+}
+console.log(childScope);
+
+function parentAndChildScope() {
+  const parentVariable = "I am in the parent's scope";
+  childScope(parentVariable);
+  console.log("Parent Scope, Parent Variable:", parentVariable);
+  console.log("Parent Scope, Child Variable:", childVariable);
+}
+parentAndChildScope;
